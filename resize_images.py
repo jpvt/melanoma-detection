a85093a8a063ef4ba5ee693b88c84e28cf5974e9
jpvt <joaoteixeira@eng.ci.ufpb.br>
2021-04-23 09:28:55 -0300

    bugfix: tqdm import

--- resize_images.py
+++ resize_images.py
@@ -1,6 +1,6 @@
 import os
 import glob
-import tqdm
+from tqdm import tqdm
 from PIL import Image, ImageFile
 from joblib import Parallel, delayed
 
@@ -20,14 +20,22 @@ input_folder = "data/jpeg/train/"
 output_folder = "data/working/train/"
 
 images = glob.glob(os.path.join(input_folder, "*.jpg"))
-Parallel(n_jobs=8)(
-    delayed(resize_image)(i, output_folder, (512, 512)) for i in tqdm(images)
+Parallel(8)(
+    delayed(resize_image)(
+        i, 
+        output_folder, 
+        (224, 224)
+    ) for i in tqdm(images)
 )
 
 input_folder = "data/jpeg/test/"
 output_folder = "data/working/test/"
 
 images = glob.glob(os.path.join(input_folder, "*.jpg"))
-Parallel(n_jobs=8)(
-    delayed(resize_image)(i, output_folder, (512, 512)) for i in tqdm(images)
+Parallel(8)(
+    delayed(resize_image)(
+        i, 
+        output_folder, 
+        (224, 224)
+    ) for i in tqdm(images)
 )

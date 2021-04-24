2f2e3f1b72e56b2f606f48606b7059d8bd390c96
jpvt <joaoteixeira@eng.ci.ufpb.br>
2021-04-24 13:28:52 -0300

    add Dockerized version of api

--- api.py
+++ api.py
@@ -14,10 +14,10 @@ from utils.data.dataset import Dataset
 
 
 app = Flask(__name__)
-UPLOADE_FOLDER = "static/data_storage"
-DEVICE = "cuda"
+UPLOAD_FOLDER = "/home/detector/app/static/"
+DEVICE = "cpu"
 MODEL = None
-MODEL_PATH = "data_storage/model_0.bin"
+MODEL_PATH = "model.bin"
 
 class SEResNext50_32x4d(nn.Module):
     """
@@ -80,20 +80,20 @@ def upload_predict():
     if request.method == "POST":
         image_file = request.files["image"]
         if image_file:
-            image_path = os.path.join(UPLOADE_FOLDER, image_file.filename)
+            image_path = os.path.join(UPLOAD_FOLDER, image_file.filename)
             image_file.save(image_path)
             pred = predict(image_path, MODEL)[0]
             print(pred)
             return render_template('index.html', prediction=round(pred, 3), image_loc = image_path)
 
-    return render_template('index.html', prediction=0, image_loc = image_path)
+    return render_template('index.html', prediction=0, image_loc = None)
 
 
 if __name__ == '__main__':
 
-    MODEL = SEResNext50_32x4d(pretrained="imagenet")
-    MODEL.load_state_dict(torch.load(MODEL_PATH))
+    MODEL = SEResNext50_32x4d(pretrained=None)
+    MODEL.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device(DEVICE)))
     MODEL.to(DEVICE)
 
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=12000, debug=True)
